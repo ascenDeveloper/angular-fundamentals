ec47715041f7eddee8d64beba0f122416d22e7d9
Ascended300 <eduardofelix014@gmail.com>
2022-08-05 13:00:31 -0300

    Components test

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,35 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.sass']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
   title = 'platzi-angular-fundamentals';
-}
+  name = 'ascend';
+  age = 20;
+  img = "https://source.unsplash.com/random";
+  btnDisabled = true;
+  person = {
+    name: 'ascend',
+    age: 18,
+    avatar: "https://source.unsplash.com/random",
+  }
+
+  toggleButton() {
+    this.btnDisabled = !this.btnDisabled;
+  }
+
+  incrementAge() {
+    this.person.age += 1;
+  }
+
+  onScroll(event: Event) {
+    const element = event.target as HTMLElement;
+    console.log(element.scrollTop);
+  }
+
+  changeName(event: Event) {
+    const element = event.target as HTMLInputElement;
+    this.person.name = element.value;
+  }
+};
